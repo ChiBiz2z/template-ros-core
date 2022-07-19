4f1504cc02697bc4c4f6d25079e9a7de9b6e32e1
Vladimir <88432358+ChiBiz2z@users.noreply.github.com>
2022-07-19 13:25:28 +0300

    Update solution.py

--- packages/circle_drive/scripts/solution.py
+++ packages/circle_drive/scripts/solution.py
@@ -1,4 +1,16 @@
+import cv2
+import numpy as np
+l_yellow_range = np.array([20, 100, 100])
+u_yellow_range = np.array([30, 255, 255])
 
 def solution(obs):
-    print(obs.shape)
-    return [-0.5, 0]
+    print(obs.shape)    
+    img_hsv = cv2.cvtColor(np.ascontiguousarray(obs), cv2.COLOR_RGB2HSV)
+    mask = cv2.inRange(img_hsv, l_yellow_range, u_yellow_range)    
+    yellow_ratio = (cv2.countNonZero(mask)) / (img_hsv.size / 3)
+    res = np.round(yellow_ratio * 100, 2)
+    
+    if(res > 0)
+        return [0.1, 0]
+    else
+        return [0, 0.5]
